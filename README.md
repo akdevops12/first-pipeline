1781abb945fcde821bad8c819daca892d7ba7b2a
ulrich <ktetsuya2022@gmail.com>
2022-08-11 13:50:32 -0400

    Fixing Error

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # first-pipeline
 first push
+second commit
--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-{
-    "name": "myapp",
-    "version": "0.0.1",
-    "private": true,
-    "scripts": {
-        "start": "node index.js"
-    },
-    "engines": {
-        "node": "^18.0.0"
-    },
-    "dependencies": {
-        "express": "^4.14.0"
-    }
-}
+var express = require('express');                
+var app = express();
+
+app.get('/', function(req, res) {                
+    res.send('Hello World!');
+});
+
+var server = app.listen(3000, function () {      
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);      
+});
